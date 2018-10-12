5479e3c6ae1f845047a14262dd7d907a17ee06ec
Jensen Judkins <thesmelliestman@gmail.com>
2018-10-12 08:57:36 -0600

    Update src/model/chatmodel.java

--- src/model/chatmodel.java
+++ src/model/chatmodel.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class chatmodel
 {
+	//Data members
+	private String currentUser;
+	private String joke;
+	private String content;
+	
+	
+	
+	
+	
+	
 	ArrayList<String> responseList = new ArrayList<String>();
 	ArrayList<String> spookyList = new ArrayList<String>();
 	
