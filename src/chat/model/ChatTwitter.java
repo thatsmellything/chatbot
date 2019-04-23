8fb49c92c370bcfa8317a30d59e62a71d538f08f
Jensen Judkins <thesmelliestman@gmail.com>
2019-04-23 09:42:00 -0600

    Update ChatTwitter.java

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,6 +1,15 @@
 package chat.model;
 
+import chat.controller.ChatController;
+
 public class ChatTwitter
 {
 
+	private ChatController app;
+	
+	public ChatTwitter(ChatController app)
+	{
+		this.app = app;
+	}
+	
 }
