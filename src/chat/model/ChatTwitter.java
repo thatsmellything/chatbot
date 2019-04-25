15655044e2cd4a9f8721e73faac0b3014c4a056e
Jensen Judkins <thesmelliestman@gmail.com>
2019-04-25 08:58:19 -0600

    Added all of the code inside of the canvas page cody gave us. BAsically the whole ChatTwitter File

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,8 +1,24 @@
 package chat.model;
 
 import chat.controller.ChatController;
-import twitter4j.Twitter;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.text.DecimalFormat;
+
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+
+import chat.controller.IOController;
 
 public class ChatTwitter
 {
@@ -10,10 +26,207 @@ public class ChatTwitter
 	private ChatController app;
 	private Twitter chatTwitter;
 	
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
+	private HashMap<String, Integer> wordsAndCount;
+	
 	public ChatTwitter(ChatController app)
 	{
 		this.app = app;
 		this.chatTwitter = TwitterFactory.getSingleton();
+		
+		this.searchedTweets = new ArrayList<Status>();
+		this.tweetedWords = new ArrayList<String>();
+		this.wordsAndCount = new HashMap<String, Integer>();
+		this.totalWordCount = 0;
 	}
 	
+
+
+	public void sendTweet(String textToTweet)
+	{
+		try
+		{
+			chatTwitter.updateStatus(textToTweet + " @ChatbotCTEC");
+		}
+		catch(TwitterException tweetError)
+		{
+			app.handleErrors(tweetError);
+		}
+		catch(Exception otherError)
+		{		
+			app.handleErrors(otherError);
+	}
+	}
+
+
+	private void collectTweets(String username)
+	{	
+		Paging statusPage = new Paging(1,100);
+		int page = 1;
+		long lastID = Long.MAX_VALUE;
+	
+		while(page <= 10)
+		{
+			statusPage.setPage(page);
+			try
+			{	
+			ResponseList<Status> listedTweets = chatTwitter.getUserTimeline(username, statusPage);
+			for(Status current : listedTweets)
+			{
+				if(current.getId() < lastID)
+				{
+					searchedTweets.add(current);
+					lastID = current.getId();
+				}
+			}
+		}
+		catch(TwitterException searchTweetError)
+			{
+			app.handleErrors(searchTweetError);
+			}
+		page++;
+		}
+	}
+	
+	private void turnStatusesToWords()
+	{
+		for(Status currentStatus : searchedTweets)
+		{
+			String tweetText = currentStatus.getText().toLowerCase();
+			tweetText = tweetText.replace("\n", " ");
+			String [] tweetWords = tweetText.split(" ");
+			for(int index = 0; index < tweetWords.length; index++)
+			{
+				tweetedWords.add(removePunctuation(tweetWords[index]).trim());
+			}
+		}
+	}
+	
+	private String removePunctuation(String currentString)
+	{
+		String punctuation = ".,'?!:;\"(){}^[]<>-"; 
+												
+		String scrubbedString = "";
+		for (int i = 0; i < currentString.length(); i++)
+		{
+			if (punctuation.indexOf(currentString.charAt(i)) == -1)
+			{
+				scrubbedString += currentString.charAt(i);
+			}	
+		}
+		return scrubbedString;
+	}
+	
+	private void removeBlanks()
+	{
+		for (int index = tweetedWords.size() - 1; index >= 0; index--)
+		{
+			if (tweetedWords.get(index).trim().length() == 0)
+			{
+				tweetedWords.remove(index);
+			}
+		}
+	}
+	
+	private String [] createIgnoredWordArray()
+	{
+		String [] boringWords;
+		String fileText = IOController.loadFromFile(app, "commonWords.txt");
+		int wordCount = 0;
+		
+		Scanner wordScanner = new Scanner(fileText);
+		
+		while(wordScanner.hasNextLine())
+		{
+			wordScanner.nextLine();
+			wordCount++;
+		}
+		
+		boringWords = new String [wordCount];
+		wordScanner.close();
+		
+		// Alternative file loading method.
+		// Uses the InputStream class
+		// Notice the lack of try/catch
+		
+		wordScanner = new Scanner(this.getClass().getResourceAsStream("data/commonWords.txt"));
+		for(int index = 0; index < boringWords.length; index++)
+		{
+			boringWords[index] = wordScanner.nextLine();
+		}
+		
+		wordScanner.close();
+		return boringWords;
+	}
+	
+	private void trimTheBoringWords(String [] boringWords)
+	{
+		for (int index = tweetedWords.size() - 1; index >= 0; index--)
+		{
+			for (int removeIndex = 0; removeIndex < boringWords.length; removeIndex++)
+			{
+				if (tweetedWords.get(index).equalsIgnoreCase(boringWords[removeIndex]))
+				{
+					tweetedWords.remove(index);
+					removeIndex = boringWords.length;
+				}
+			}
+		}
+	}
+	
+	private void generateWordCount()
+	{
+		for (String word : tweetedWords)
+		{
+			if (!wordsAndCount.containsKey(word.toLowerCase()))
+			{
+				wordsAndCount.put(word.toLowerCase(), 1);
+			}
+			else
+			{
+				wordsAndCount.replace(word.toLowerCase(), wordsAndCount.get(word.toLowerCase()) + 1);
+			}
+		}
+	}
+	
+	private ArrayList<Map.Entry<String, Integer>> sortHashMap()
+	{
+		ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(wordsAndCount.entrySet());
+		entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+		
+		return entries;
+	}
+	
+	public String getMostCommonWord(String username)
+	{
+		String mostCommon = "";
+		
+		collectTweets(username);
+		turnStatusesToWords();
+		totalWordCount = tweetedWords.size();
+		String [] boring = createIgnoredWordArray();
+		removeBlanks();
+		trimTheBoringWords(boring);
+		generateWordCount();
+
+		ArrayList<Map.Entry<String, Integer>> sorted = sortHashMap();	    
+		String mostCommonWord = sorted.get(0).getKey();
+		int maxWord = 0;
+		
+		maxWord = sorted.get(0).getValue();
+
+mostCommon = "The most common word in " + username + "'s "+ searchedTweets.size() + " tweets is " +
+					mostCommonWord + ", and it was used " + maxWord + " times.\nThis is " + 
+					(DecimalFormat.getPercentInstance().format(((double) maxWord) / totalWordCount)) + 
+					" of total words: " + totalWordCount + " and is " + 
+					(DecimalFormat.getPercentInstance().format(((double) maxWord) / wordsAndCount.size())) +
+					" of the unique words: " + wordsAndCount.size() ;
+		
+		mostCommon += "\n\n" + sortedWords();
+		
+		return mostCommon;
+	}
+
 }
