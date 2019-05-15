e61c32ad1f5d5e9c6f21473c49f074a5c7d71dce
Jensen Judkins <thesmelliestman@gmail.com>
2019-05-15 09:15:47 -0600

    Update ChatTwitter.java

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -223,8 +223,8 @@ public class ChatTwitter
 		
 		maxWord = sorted.get(0).getValue();
 
-		mostCommon = "The most common word in " + username + "'s "+ searchedTweets.size() + " tweets is " +
-					mostCommonWord + ", and it was used " + maxWord + " times.\nThis is " + 
+		mostCommon = "The most common word in " + username + "'s "+ searchedTweets.size() + " tweets is '" +
+					mostCommonWord + "', and it was used " + maxWord + " times.\nThis is " + 
 					(DecimalFormat.getPercentInstance().format(((double) maxWord) / totalWordCount)) + 
 					" of total words: " + totalWordCount + " and is " + 
 					(DecimalFormat.getPercentInstance().format(((double) maxWord) / wordsAndCount.size())) +
