5b65dd13ed29c402a09c4fe8f58f5d3c38b35be7
thatsmellything <thesmelliestman@gmail.com>
2019-05-19 03:16:56 -0600

    got rid of useless extra panel thingy

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -120,7 +120,6 @@ public class ChatPanel extends JPanel
 		buttonPanelBottom.setBackground(Color.GRAY);
 		this.setBackground(Color.GRAY);
 		this.add(chatPane_1);
-		this.add(following);
 		this.add(chatField);
 		this.add(buttonPanelTop);
 		this.add(buttonPanelBottom);
@@ -143,8 +142,6 @@ public class ChatPanel extends JPanel
 		fArea.setEditable(false);
 		fArea.setLineWrap(false);
 		fArea.setWrapStyleWord(true);
-		following.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		following.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		
 	}
 	
@@ -296,8 +293,6 @@ public class ChatPanel extends JPanel
 		chatArea.setToolTipText("This is where the majority of the text goes from using the program. Only important information will be displayed on the screen to the left. The rest of it will be here :)");
 		chatArea.setForeground(Color.GREEN);
 		chatArea.setBackground(Color.DARK_GRAY);
-		
-		following = new JScrollPane();
 		
 		chatPane_1 = new JScrollPane();
 		appLayout.putConstraint(SpringLayout.WEST, buttonPanelTop, 0, SpringLayout.WEST, chatPane_1);
